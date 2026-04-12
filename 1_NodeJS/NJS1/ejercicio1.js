1bd361cfe5a7f23785a9955046eda0e248d7f6f5
NachoTorress <163569982+NachoTorress@users.noreply.github.com>
2026-04-12 18:20:00 -0300

    Implement HTTP server in ejercicio1.js
    Replaced console log statements with an HTTP server implementation that responds with 'Hola Mundo' and 'Fin'.

--- 1_NodeJS/NJS1/ejercicio1.js
+++ 1_NodeJS/NJS1/ejercicio1.js
@@ -1,3 +1,13 @@
-// Console log para mostrar "Hola Mundo" y "Fin"
-console.log("Hola Mundo");
-console.log("Fin");
+// ejercicio1.js
+import { createServer } from 'node:http';
+
+const server = createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  
+});
+server.listen(3000, '127.0.0.1', () => {
+  console.log("Hola Mundo");
+  console.log("Fin");
+});
+
+// run with `node ejercicio1.js`
