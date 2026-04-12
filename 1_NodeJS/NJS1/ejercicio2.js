a793c26a44a460c1939aabf5ae4dc2777916a801
NachoTorress <163569982+NachoTorress@users.noreply.github.com>
2026-04-12 18:20:33 -0300

    Implement basic HTTP server with math operation logs
    Added a simple HTTP server that logs basic math operations.

--- 1_NodeJS/NJS1/ejercicio2.js
+++ 1_NodeJS/NJS1/ejercicio2.js
@@ -1,8 +1,23 @@
+// ejercicio2.js
+import { createServer } from 'node:http';
 
-// Console log para mostrar el resultado de operaciones matemáticas básicas
-console.log("Resultado de la suma de 4 y 5: "+ (4+5));
-console.log("Resultado de la resta de 6 a 3: "+ (3-6));
-console.log("Resultado de la multiplicación de 2 por 7: "+ (2*7));
-console.log("Resultado de la división de 20 entre 4: "+ (20/4));
+const server = createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  
+});
+
+// starts a simple http server locally on port 3000
+server.listen(3000, '127.0.0.1', () => {
+  // Console log para mostrar el resultado de operaciones matemáticas básicas
+    console.log("Resultado de la suma de 4 y 5: "+ (4+5));
+    console.log("Resultado de la resta de 6 a 3: "+ (3-6));
+    console.log("Resultado de la multiplicación de 2 por 7: "+ (2*7));
+    console.log("Resultado de la división de 20 entre 4: "+ (20/4));
+});
+
+// run with `ejercicio2.js`
+
+
+
 
 
