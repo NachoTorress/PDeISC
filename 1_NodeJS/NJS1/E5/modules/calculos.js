17ec30395f384ed4111033665d3696bd7226668f
NachoTorress <ignaciogatorres@gmail.com>
2026-05-20 11:13:00 -0300

    Correcion NJS1

---
+++ 1_NodeJS/NJS1/E5/modules/calculos.js
@@ -0,0 +1,15 @@
+//Creación de funciones para realizar operaciones matemáticas básicas
+function suma(a, b) {
+    return a + b;
+}
+function resta(a, b) {
+    return a - b;
+}
+function multiplicacion(a, b) {
+    return a * b;
+}
+function division(a, b) {
+    return a / b;
+}
+// Exportación de las funciones para su uso en otros archivos
+export {suma, resta, multiplicacion, division};
