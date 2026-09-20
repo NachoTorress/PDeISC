6e2f0deb7675f8462d5a93c3c56b06ea67adcf0f
NachoTorress <163569982+NachoTorress@users.noreply.github.com>
2026-09-19 22:34:19 -0300

    Wrap Layout inside AuthProvider and DataProvider

--- 3_React/R4/src/App.tsx
+++ 3_React/R4/src/App.tsx
@@ -1,19 +1,29 @@
+/**
+ * Root Application Component.
+ * Integrates theme providers, authentication context, data context, and global layout.
+ */
 import { Layout } from './components/layout/Layout';
 import { GlobalStyles } from './styles/GlobalStyles';
 import { ThemeProvider } from '@emotion/react';
 import { theme } from './styles/theme';
 import { PortfolioThemeProvider } from './contexts/ThemeContext';
+import { AuthProvider } from './contexts/AuthContext';
+import { DataProvider } from './contexts/DataContext';
 import { PortfolioPage } from './pages/PortfolioPage';
 
 function App() {
   return (
     <PortfolioThemeProvider>
-      <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <Layout>
-          <PortfolioPage />
-        </Layout>
-      </ThemeProvider>
+      <AuthProvider>
+        <DataProvider>
+          <ThemeProvider theme={theme}>
+            <GlobalStyles />
+            <Layout>
+              <PortfolioPage />
+            </Layout>
+          </ThemeProvider>
+        </DataProvider>
+      </AuthProvider>
     </PortfolioThemeProvider>
   );
 }
