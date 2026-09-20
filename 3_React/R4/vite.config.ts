22f0f5535ee1fa2fd935b3e87c4a17524672af59
NachoTorress <163569982+NachoTorress@users.noreply.github.com>
2026-09-19 22:31:49 -0300

    Fix Emotion babel plugin dependency in vite config

--- 3_React/R4/vite.config.ts
+++ 3_React/R4/vite.config.ts
@@ -7,9 +7,7 @@ export default defineConfig({
   base: './',
   plugins: [
     react({
-      babel: {
-        plugins: ['@emotion/babel-plugin']
-      }
+      jsxImportSource: '@emotion/react',
     }),
     visualizer({
       open: false,
